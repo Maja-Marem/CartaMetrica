d27b095583bd8e0844e3d709582deed2bdfbf4b5
Maja-Marem <mbpagadora@up.edu.ph>
2024-05-14 21:10:24 +0800

    polishing functions

--- functions.js
+++ functions.js
@@ -54,11 +54,49 @@ function getEasting (E_Prev, departure){
 }
 
 function getLEC (Sum_Lat, Sum_Dep) {
+    /*
+    calculate the LEC using Sum_Lat and Sum_Dep
+    */
     let LEC = sqrt((pow(Sum_Lat, 2)) + (pow(Sum_Dep,2)))
     return LEC
 }
 
 function getBoSE (Sum_Lat, Sum_Dep){
-    let BoSE = atan(Sum_Dep/Sum_Lat)
-    return BoSE
+    /*
+    Calculating BoSE using Sum_Lat and Sum_Dep
+    */
+    let BoSE_mag = atan(Sum_Dep/Sum_Lat)
+    if (Sum_Lat < 0 && Sum_Dep < 0){
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        BoSE = "N" + BoSE_dms + "E"
+        return BoSE
+    }
+    else if (Sum_Lat < 0 && Sum_Dep > 0){
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        BoSE = "N" + BoSE_dms + "W"
+        return BoSE
+    }
+    else if (Sum_Lat > 0 && Sum_Dep > 0){
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        BoSE = "S" + BoSE_dms + "W"
+        return BoSE
+    }
+    else (Sum_Lat > 0 && Sum_Dep < 0){
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        BoSE = "S" + BoSE_dms + "E"
+        return BoSE
+    }
 }
+
