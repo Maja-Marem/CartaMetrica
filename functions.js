15b567ed74b8fe590b4ef06818724b4a878cdede
Maja-Marem <mbpagadora@up.edu.ph>
2024-05-14 21:33:01 +0800

    polished functions

--- functions.js
+++ functions.js
@@ -65,38 +65,41 @@ function getBoSE (Sum_Lat, Sum_Dep){
     /*
     Calculating BoSE using Sum_Lat and Sum_Dep
     */
-    let BoSE_mag = atan(Sum_Dep/Sum_Lat)
+    let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
     if (Sum_Lat < 0 && Sum_Dep < 0){
         let BoSE_degs = parseInt(BoSE_mag)
         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-        BoSE = "N" + BoSE_dms + "E"
+        let BoSE = "N" + BoSE_dms + "E"
         return BoSE
     }
     else if (Sum_Lat < 0 && Sum_Dep > 0){
         let BoSE_degs = parseInt(BoSE_mag)
         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-        BoSE = "N" + BoSE_dms + "W"
+        let BoSE = "N" + BoSE_dms + "W"
         return BoSE
     }
     else if (Sum_Lat > 0 && Sum_Dep > 0){
         let BoSE_degs = parseInt(BoSE_mag)
         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-        BoSE = "S" + BoSE_dms + "W"
+        let BoSE = "S" + BoSE_dms + "W"
         return BoSE
     }
-    else (Sum_Lat > 0 && Sum_Dep < 0){
+    else if (Sum_Lat > 0 && Sum_Dep < 0){
         let BoSE_degs = parseInt(BoSE_mag)
         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-        let BoSE_secs = ((((az - degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-        BoSE = "S" + BoSE_dms + "E"
+        let BoSE = "S" + BoSE_dms + "E"
+        return BoSE
+    }
+    else {
+        let BoSE = "EWAN"
         return BoSE
     }
 }
-
