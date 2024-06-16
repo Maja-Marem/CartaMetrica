7f5f1fd4973f010d286c3dd61cd62cf03433da30
John Austin S. Macaraeg <jsmacaraeg1@up.edu.ph>
2024-06-16 17:20:13 +0800

    added some containers pati yung logo

--- CartaMetrica/screens/Main.js
+++ CartaMetrica/screens/Main.js
@@ -1,6 +1,10 @@
 // screens/Main.js
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import Cartametrica_Logo from './pictures/Cartametrica_Logo.png'
+
+const blurhash =
+'|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 const Main = ({navigation}) => {
   const [lines, setLines] = useState([]);
@@ -18,7 +22,16 @@ const Main = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      <Text>Welcome to CartaMetrica</Text>
+      <View style={styles.logo}>
+        <Image
+          style={styles.image}
+          source={Cartametrica_Logo}
+          placeholder={blurhash}
+          contentFit="cover"
+          transition={1000}
+        />
+      </View>
+
       {lines.length > 0 && (
         <View style={styles.linesContainer}>
           {lines.map((line, index) => (
@@ -26,6 +39,7 @@ const Main = ({navigation}) => {
           ))}
         </View>
       )}
+
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
@@ -52,6 +66,13 @@ const Main = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
+  logo:{
+    width: '100%',
+    height: '40%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#efefd7',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
--- CartaMetrica/screens/Plotting.js
+++ CartaMetrica/screens/Plotting.js
@@ -1,11 +1,32 @@
 // screens/Plotting.js
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import Cartametrica_Logo from './pictures/Cartametrica_Logo.png'
+
+const blurhash =
+'|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 const Plotting = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Plotting Screen</Text>
+      <View style={styles.box1}>
+        <Image
+          style={styles.image}
+          source={Cartametrica_Logo}
+          placeholder={blurhash}
+          contentFit="cover"
+          transition={1000}
+        />
+      </View>
+
+      <View style={styles.box2}>
+        <Text>PLOTTED TRAVERSE</Text>
+      </View>
+
+      <View style={styles.box3}>
+
+      </View>
+        
       <Button
         title="Go to Welcome"
         onPress={() => navigation.navigate('Welcome')}
@@ -15,3 +36,21 @@ const Plotting = ({ navigation }) => {
 };
 
 export default Plotting;
+
+const styles = StyleSheet.create({
+  box1: {
+    width: '100%',
+    height: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#efefd7',
+  },
+  box2: { 
+    width: '100%',
+    height: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10, 
+    backgroundColor: '#efefd7',
+  }
+});
