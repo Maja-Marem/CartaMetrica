bb1e6c42fb09d6af674623a0ca10fdb31db0b5fc
Maja-Marem <mbpagadora@up.edu.ph>
2024-06-17 17:40:24 +0800

    polishing core functions

--- CartaMetrica/core/function.js
+++ CartaMetrica/core/function.js
@@ -87,73 +87,133 @@ function getCoords(lines){
         
     return Coordinates
 }
+// CALCULATIG CORRECTIONS
+export function getLEC (lines) {
+    /*
+    
+    */
 
-export function getLEC (Sum_Lat, Sum_Dep) {
-     /*
-     calculate the LEC using Sum_Lat and Sum_Dep
-     */
-     let LEC = sqrt((pow(Sum_Lat, 2)) + (pow(Sum_Dep,2)))
-     return LEC
- }
+    // list of calculated latitudes
+    let lat_of_lines = [];
+    
+    // calculate latitude for each line
+    for (let i = 0; i < lines.length; i++) {
+        let distance = lines[i][0];
+        let azimuth_n = lines[i][1];
+        let latitude = (distance*cos(radians*azimuth_n))
+        lat_of_lines.push(latitude);
+    }
 
-export function getBoSE (Sum_Lat, Sum_Dep){
-     /*
-     Calculating BoSE using Sum_Lat and Sum_Dep
-     */
-     if (Sum_Lat < 0 && Sum_Dep < 0){
-         let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
-         let BoSE_degs = parseInt(BoSE_mag)
-         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-         let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
-         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-         let BoSE = "N" + BoSE_dms + "E"
-         return BoSE
-     }
-     else if (Sum_Lat < 0 && Sum_Dep > 0){
-         let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
-         let BoSE_degs = parseInt(BoSE_mag)
-         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-         let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
-         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-         let BoSE = "N" + BoSE_dms + "W"
-         return BoSE
-     }
-     else if (Sum_Lat > 0 && Sum_Dep > 0){
-         let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
-         let BoSE_degs = parseInt(BoSE_mag)
-         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-         let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
-         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-         let BoSE = "S" + BoSE_dms + "W"
-         return BoSE
-     }
-     else if (Sum_Lat > 0 && Sum_Dep < 0){
-         let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
-         let BoSE_degs = parseInt(BoSE_mag)
-         let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
-         let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
-         let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
-         let BoSE = "S" + BoSE_dms + "E"
-         return BoSE
-     }
-     else if (Sum_Lat == 0 && Sum_Dep < 0){
-         let BoSE = "Due East"
-         return BoSE
-     }
-     else if (Sum_Lat == 0 && Sum_Dep > 0){
-         let BoSE = "Due West"
-         return BoSE
-     }
-     else if (Sum_Dep == 0 && Sum_Lat < 0){
-         let BoSE = "Due North"
-         return BoSE
-     }
-     else if (Sum_Dep == 0 && Sum_Lat > 0){
-         let BoSE = "Due South"
-         return BoSE
-     }
-     else {
-         let BoSE = "EWAN"
-         return BoSE
-     }
- }
+    // list of cumulative LatSums
+    let LatSum = 0
+
+    // calculating LatSum
+    for (let i = 0; i < lat_of_lines.length; i++) {
+        LatSum += lat_of_lines [i] ;
+    }
+        
+// list of calculated departures
+let dep_of_lines = [];
+    
+// calculate latitude for each line
+for (let i = 0; i < lines.length; i++) {
+    let distance = lines[i][0];
+    let azimuth_n = lines[i][1];
+    let departure = (distance*sin(radians*azimuth_n))
+    dep_of_lines.push(departure);
+}
+
+// list of cumulative LatSums
+let DepSum = 0
+
+// calculating LatSum
+for (let i = 0; i < dep_of_lines.length; i++) {
+    DepSum += dep_of_lines [i] ;
+}
+
+    let LEC = sqrt((pow(LatSum, 2)) + (pow(DepSum,2)))
+    return LEC
+} 
+
+function getREC (LEC, Sum_Dist) {
+    let cREC = SumDist/LEC
+    let REC = "1:"+ cREC
+    return REC
+}
+
+function getBoSE (Sum_Lat, Sum_Dep){
+    /*
+    Calculating BoSE using Sum_Lat and Sum_Dep
+    */
+
+    if (Sum_Lat < 0 && Sum_Dep < 0){
+        let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        let BoSE = "N" + BoSE_dms + "E"
+        return BoSE
+    }
+    else if (Sum_Lat < 0 && Sum_Dep > 0){
+        let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        let BoSE = "N" + BoSE_dms + "W"
+        return BoSE
+    }
+    else if (Sum_Lat > 0 && Sum_Dep > 0){
+        let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        let BoSE = "S" + BoSE_dms + "W"
+        return BoSE
+    }
+    else if (Sum_Lat > 0 && Sum_Dep < 0){
+        let BoSE_mag = atan(Sum_Dep/Sum_Lat)*degrees
+        let BoSE_degs = parseInt(BoSE_mag)
+        let BoSE_mins = parseInt((BoSE_mag - BoSE_degs)*60)
+        let BoSE_secs = ((((BoSE_mag - BoSE_degs)*60)- BoSE_mins)*60).toFixed(2)
+        let BoSE_dms = BoSE_degs + "-" + BoSE_mins + "-" + BoSE_secs
+        let BoSE = "S" + BoSE_dms + "E"
+        return BoSE
+    }
+    else if (Sum_Lat == 0 && Sum_Dep < 0){
+        let BoSE = "Due East"
+        return BoSE
+    }
+    else if (Sum_Lat == 0 && Sum_Dep > 0){
+        let BoSE = "Due West"
+        return BoSE
+    }
+    else if (Sum_Dep == 0 && Sum_Lat < 0){
+        let BoSE = "Due North"
+        return BoSE
+    }
+    else if (Sum_Dep == 0 && Sum_Lat > 0){
+        let BoSE = "Due South"
+        return BoSE
+    }
+    else {
+        let BoSE = "EWAN"
+        return BoSE
+    }
+}
+    
+var data = [
+    [13.23, 124.795], 
+    [15.57, 14.143], 
+    [43.36, 270.000], 
+    [23.09, 188.169], 
+    [10.99, 180.000], 
+    [41.40, 50.562], 
+]
+
+Coordinates = getCoords(data)
+console.log(Coordinates)
+
+
